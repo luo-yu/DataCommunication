5ab42ea5b062d73df5e2373e4752ad2f3c86b23b
luo-yu <luoy6@miamioh.edu>
2013-10-28 15:41:05 -0400

    chat server

---
+++ ChatStart/src/ChatServer.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+
+
+public class ChatServer {
+	private Socket socket = null;
+	private ServerSocket serverSocket = null;
+	private DataInputStream dis = null;
+	private DataOutputStream dos = null;
+	public final int  port = 6789;
+	
+	public ChatServer(){
+		try {
+			serverSocket = new ServerSocket(port);
+			socket = serverSocket.accept();
+		} catch (IOException e) {
+			System.err.println("Unable to create server socket");
+		}
+	}
+	
+	
+	public void makeStreams(){
+		try {
+			dis = new DataInputStream(socket.getInputStream());
+			dos = new DataOutputStream(socket.getOutputStream());
+		} catch (IOException e) {
+			System.err.println("Unable to make streams");
+		}
+	}
+}
