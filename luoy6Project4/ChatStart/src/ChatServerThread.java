2f132da48b318ba7225afe03f800700e1b634de1
luo-yu <luoy6@miamioh.edu>
2013-11-04 10:59:05 -0500

    Client and server

--- luoy6Project4/ChatStart/src/ChatServerThread.java
+++ luoy6Project4/ChatStart/src/ChatServerThread.java
@@ -1,49 +1,61 @@
-import java.io.*;
 import java.net.*;
-
+import java.io.*;
 
 public class ChatServerThread extends Thread {
-	
-	
+	private ChatServer server = null;
 	private Socket socket = null;
-	private int iD = -1;
-	private DataInputStream dis = null;
-	private DataOutputStream dos = null;
-	
-	
-	protected ChatServerThread(Socket socket){
-		this.socket = socket;
-		this.iD = socket.getPort();
+	private int ID = -1;
+	private DataInputStream streamIn = null;
+	private DataOutputStream streamOut = null;
+
+	public ChatServerThread(ChatServer _server, Socket _socket) {
+		super();
+		server = _server;
+		socket = _socket;
+		ID = socket.getPort();
 	}
-	
-	public void run(){
-		
-	}
-	
-	/**
-	 * Creates BufferReader, DataInputStream, and DataoutputStream
-	 */
-	protected void makeStream() {
+
+	public void send(String msg) {
 		try {
-			dis = new DataInputStream(socket.getInputStream());
-			dos = new DataOutputStream(socket.getOutputStream());
-		} catch (IOException e) {
-			System.err.println("Unable to make streams");
-		}
-	}
-	
-	/**
-	 * Closes all the streams, socket, and BufferReader
-	 */
-	protected void closeConnection() {
-		try {
-			this.dis.close();
-			this.dos.close();
-			this.socket.close();
-		} catch (IOException e) {
-			System.err.println("Unable to close connections");
+			streamOut.writeUTF(msg);
+			streamOut.flush();
+		} catch (IOException ioe) {
+			System.out.println(ID + " ERROR sending: " + ioe.getMessage());
+			server.remove(ID);
+			stop();
 		}
 	}
 
+	public int getID() {
+		return ID;
+	}
 
+	public void run() {
+		System.out.println("Server Thread " + ID + " running.");
+		while (true) {
+			try {
+				server.handle(ID, streamIn.readUTF());
+			} catch (IOException ioe) {
+				System.out.println(ID + " ERROR reading: " + ioe.getMessage());
+				server.remove(ID);
+				stop();
+			}
+		}
+	}
+
+	public void open() throws IOException {
+		streamIn = new DataInputStream(new BufferedInputStream(
+				socket.getInputStream()));
+		streamOut = new DataOutputStream(new BufferedOutputStream(
+				socket.getOutputStream()));
+	}
+
+	public void close() throws IOException {
+		if (socket != null)
+			socket.close();
+		if (streamIn != null)
+			streamIn.close();
+		if (streamOut != null)
+			streamOut.close();
+	}
 }
