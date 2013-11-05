fbacea558b1bb2d04d2535a192452be996b48df6
luo-yu <luoy6@miamioh.edu>
2013-11-05 12:01:57 -0500

    Something

--- luoy6Project4/ChatStart/src/ChatClient.java
+++ luoy6Project4/ChatStart/src/ChatClient.java
@@ -7,6 +7,9 @@ public class ChatClient {
 	private DataInputStream dis = null;
 	private DataOutputStream dos = null;
 	private Scanner operation = null;
+	private InetSocketAddress inetSocketAddress = null;
+	private ServerSocket listenSocket = null;
+	private int listenPort = 0;
 
 	/**
 	 * 
@@ -14,30 +17,78 @@ public class ChatClient {
 	 * @param serverPort
 	 */
 	protected ChatClient(InetAddress address, int serverPort) {
+		try {
+			listenSocket = new ServerSocket(0);
+		} catch (IOException e1) {
+			System.err.println("Unable to listen to peer");
+		}
 		System.out.println("Establishing connection...");
 		try {
 			socket = new Socket(address, serverPort);
 		} catch (IOException e) {
 			System.err.println("Unable to create socket");
 		}
+		listening();
 		this.makeStreams();
 		sendOperation();
 
 	}
 
+	private void listening() {
+		do {
+			try {
+				new ChatClientThread(listenSocket.accept()).start();
+			} catch (IOException e) {
+				System.err.println("Terminating with client");
+			}
+		} while (true);
+
+	}
+
 	private void sendOperation() {
 		System.out
-				.println("If you want to add yourself to the server, enter \'add\' followed by a screen name; \n"
-						+ "if you want to remove yourself from the server, enter \'remove\' followed by a screen name;\n"
-						+ "if you want to request an address of a user, enter \'request\' followed by a screen name"
-						+ "if you want to quit, enter \'quit\'");
-		 operation= new Scanner(System.in);
-		 String temp = operation.nextLine();
-		 
-		 try {
-			dos.writeUTF(temp);
+				.print("If you want to add yourself to the server, enter 10'; \n"
+						+ "if you want to remove yourself from the server, enter 20' ;\n"
+						+ "if you want to request an address of a user, enter 30 "
+						+ "if you want to quit, enter 40");
+		operation = new Scanner(System.in);
+		int operationCode = operation.nextInt();
+
+		try {
+			dos.writeInt(operationCode);
 		} catch (IOException e) {
 			System.err.println("Unable to write operation");
+		}
+
+		if (operationCode == ChatServer.ADD_NAME_IP) {
+			System.out.print("Please enter your screen name: ");
+			String name = operation.nextLine();
+			System.out.print("Please enter your ip address: ");
+			String ipaddress = operation.next();
+			try {
+				dos.writeUTF(name);
+			} catch (IOException e) {
+				System.err.println("Unable to write name");
+			}
+			listenPort = listenSocket.getLocalPort();
+			try {
+				inetSocketAddress = new InetSocketAddress(
+						InetAddress.getByName(ipaddress), listenPort);
+			} catch (UnknownHostException e) {
+				System.err.println("Unable to create inet socket address");
+			}
+			byte[] ip = inetSocketAddress.getAddress().getAddress();
+			try {
+				dos.write(ip);
+			} catch (IOException e) {
+				System.err.println("Unable to write IP to server");
+			}
+			try {
+				dos.writeInt(listenPort);
+			} catch (IOException e) {
+				System.err.println("Unable to write port number");
+			}
+
 		}
 
 	}
@@ -65,7 +116,24 @@ public class ChatClient {
 
 	public static void main(String args[]) throws NumberFormatException,
 			UnknownHostException {
-		new ChatClient(InetAddress.getByName("127.0.0.1"), ChatServer.port);
+		new ChatClient(InetAddress.getByName("127.0.0.1"),
+				ChatServer.SERVER_PORT);
+	}
+
+	protected class ChatClientThread extends Thread {
+		public ChatClientThread(Socket socket) {
+
+		}
+
+		/**
+		 * 
+		 * Calling this method will causes a separate executing thread. It
+		 * contains all the methods needed to interact with the chat server
+		 */
+		public void run() {
+
+		}// end run
+
 	}
 
 }
--- luoy6Project4/ChatStart/src/ChatServer.java
+++ luoy6Project4/ChatStart/src/ChatServer.java
@@ -3,101 +3,184 @@ import java.net.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServer {
+	private DataInputStream dis = null;
+	private DataOutputStream dos = null;
 	private Socket socket = null;
 	private ServerSocket serverSocket = null;
-	private DataInputStream dis = null;
 	private boolean quit = false;
-	public static final int port = 6789;
-	ConcurrentHashMap<String, InetSocketAddress> clientMap = null;
+	public static final int SERVER_PORT = 6789;
+	protected ConcurrentHashMap<String, InetSocketAddress> clientMap = null;
 	private String operation = "";
+	protected boolean listen = true;
+	public static final int ADD_NAME_IP = 10;
+	public static final int REMOVE_NAME_IP = 20;
+	public static final int REQUEST = 30;
+	public static final int QUIT = 40;
+	private InetSocketAddress inetSocketAddress = null;
 
-	protected ChatServer(int port) {
+	/**
+	 * Constructs and initializes chat server. It initializes client map to
+	 * store client information. It creates a ServerSocket to use for listening
+	 * for clients.
+	 */
+	protected ChatServer() {
 		clientMap = new ConcurrentHashMap<String, InetSocketAddress>();
 
 		try {
-			serverSocket = new ServerSocket(port);
+			serverSocket = new ServerSocket(SERVER_PORT);
 		} catch (IOException e) {
-			System.err.println("Unable to create server socket");
+
+			System.err.println("Could not listen on port: " + SERVER_PORT);
+			System.exit(-1);
 		}
+		listening();
+
 		try {
-			socket = serverSocket.accept();
-			System.out.println("Client accepted: " + socket);
+			serverSocket.close();
 		} catch (IOException e) {
-			System.err.println("Unable to accept socket");
-		}
-		this.makeStreams();
-		try {
-			operation = dis.readUTF();
-			System.out.println("Operation string: " + operation);
-		} catch (IOException e) {
-			System.err.println("Unable to read operation");
-		}
-
-		handleClientOperation(operation);
-
-	}
-
-	protected void handleClientOperation(String temp) {
-		int length = temp.length();
-
-		if (temp.substring(0, 3).equalsIgnoreCase("add")) {
-			System.out.println("Inside of add");
-			addUsertoMap();
-		} else if (temp.substring(0, 4).equalsIgnoreCase("quit")) {
-			System.out.println("Inside of quit");
-			quit();
-		} else if (temp.substring(0, 6).equalsIgnoreCase("remove")) {
-			System.out.println("Inside of remove");
-			removeUser();
-		} else {
-			System.out.println("Inside of request");
-			handleUserRequest();
+			System.err.println("Unable to close serverSoccket.");
 		}
 
 	}
 
-	private void quit() {
-		// TODO Auto-generated method stub
+	protected void listening() {
+		do {
+			// Create new thread to hand each client.
+			// Pass the Socket object returned by the accept
+			// method to the thread.
+			try {
+
+				new ChatServerThread(serverSocket.accept()).start();
+			} catch (IOException e) {
+				System.err.println("Terminating with client");
+			}
+		} while (listen);
 
 	}
 
-	private void handleUserRequest() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void removeUser() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void addUsertoMap() {
-		// TODO Auto-generated method stub
-
-	}
-
-	protected void makeStreams() {
-		try {
-			dis = new DataInputStream(socket.getInputStream());
-		} catch (IOException e) {
-			System.err.println("Unable to make stream");
-		}
-	}
-
-	protected void closeConnections() {
-		try {
-			dis.close();
-			socket.close();
-		} catch (IOException e) {
-			System.err.println("Unable to close connections");
-		}
-	}
-
+	/**
+	 * Starts a chat server
+	 * 
+	 * @param args
+	 */
 	public static void main(String args[]) {
-		new ChatServer(port);
+		new ChatServer();
 	}
 
-	protected class serviceClient extends Thread {
+	protected class ChatServerThread extends Thread {
+
+		/**
+		 * Sets up class data members. Displays communication info.
+		 */
+		public ChatServerThread(Socket socket) {
+			super();
+
+		}
+
+		/**
+		 * 
+		 * Calling this method will causes a separate executing thread. It
+		 * contains all the methods needed to interact with the chat server
+		 */
+		public void run() {
+
+		}// end run
+
+		/**
+		 * Create streams to a client
+		 */
+		protected void makeStreams() {
+			try {
+				dis = new DataInputStream(socket.getInputStream());
+				dos = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+				System.err.println("Unable to make stream");
+			}
+		}
+
+		/**
+		 * Close the connection to a client
+		 */
+		protected void closeConnections() {
+			try {
+				dis.close();
+				socket.close();
+			} catch (IOException e) {
+				System.err.println("Unable to close connections");
+			}
+		}
+
+		/**
+		 * Handles client operations
+		 */
+		protected void handleClientOperation() {
+			int operation = 0;
+			try {
+				operation = dis.readInt();
+			} catch (IOException e) {
+				System.err.println("Can't read operation code");
+			}
+
+			if (operation == ADD_NAME_IP) {
+				System.out.println("Inside of add");
+				addUsertoMap();
+			} else if (operation == QUIT) {
+				System.out.println("Inside of quit");
+				quit();
+			} else if (operation == REMOVE_NAME_IP) {
+				System.out.println("Inside of remove");
+				removeUser();
+			} else {
+				System.out.println("Inside of request");
+				handleUserRequest();
+			}
+
+		}
+
+		private void quit() {
+			// TODO Auto-generated method stub
+
+		}
+
+		private void handleUserRequest() {
+			// TODO Auto-generated method stub
+
+		}
+
+		private void removeUser() {
+			// TODO Auto-generated method stub
+
+		}
+
+		private void addUsertoMap() {
+			String name="";
+			int port=0;
+			try {
+				name = dis.readUTF();
+			} catch (IOException e) {
+				System.err.println("Unable to read name");
+			}
+			
+			byte[] ip = new byte[4]; 
+			 try {
+				dis.read(ip);
+			} catch (IOException e) {
+				System.err.println("Unable to read ip");
+			} 
+			 
+			 try {
+				port = dis.readInt();
+			} catch (IOException e1) {
+				System.err.println("Unable to read port number");
+			}
+
+			 try {
+				inetSocketAddress = new InetSocketAddress(InetAddress.getByAddress(ip),port);
+			} catch (UnknownHostException e) {
+				System.err.println("Unable to get inet socket address");
+			}
+			 clientMap.put(name,inetSocketAddress);
+		}
 
 	}
 
